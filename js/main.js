ec6276ad1198ba6c655fd5c4d053a39ae8d04be2
tpmghy <mura954jp@gmail.com>
2026-01-29 21:16:47 +0900

    レインコート風サイト

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.mobile-menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (menuToggle && mainNav) {
+        menuToggle.addEventListener('click', () => {
+            mainNav.classList.toggle('open');
+        });
+    }
+});
